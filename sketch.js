4a5059d0e3a11227c83248d2ef6bf12652e96b57
chrischne <chrischne@gmail.com>
2022-10-28 17:18:35 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,3 +1,11 @@
+let pos = {
+  lat: 0,
+  lng: 0
+}
+
+let nogeo = false;
+let init = false;
+
 // must be in HTTPS
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -6,25 +14,59 @@ function setup() {
   // get position once
 
   if (!navigator.geolocation) {
-    background(255,0,0);
-    text("navigator.geolocation is not available",30,30);
+    nogeo = true;
   }
   navigator.geolocation.getCurrentPosition(setPos);
   setInterval(function () {
     navigator.geolocation.getCurrentPosition(setPos);
-  }, 1000)
+  }, 500)
+
+
+  noLoop();
+}
+
+function draw() {
+
+  if (nogeo) {
+    background(255, 0, 0);
+    fill(0);
+    noStroke();
+    text("navigator.geolocation is not available", 30, 30);
+    return;
+  }
+
+  if(
+    !init
+  ){
+    background(255, 255, 0);
+    fill(0);
+    noStroke();
+    text("waiting for position", 30, 30);
+    return;
+  }
+
+  background(0);
+  fill(255);
+  textSize(32);
+  text(
+    "Lat: " + pos.lat + "\nLon: " + pos.lng,
+    30,
+    50
+  );
+
 
 }
 
 function setPos(position) {
   let lat = position.coords.latitude;
   let lng = position.coords.longitude;
-  background(0);
-  fill(255);
-  textSize(32);
-  text(
-    "Lat: " + lat + "\nLon: " + lng,
-    10,
-    height / 2
-  );
+
+  pos.lat = lat;
+  pos.lng = lng;
+
+  init = true;
+
+  redraw();
+
+
 }
